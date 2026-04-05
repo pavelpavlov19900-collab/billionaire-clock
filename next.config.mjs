b32d6f66d6ccdb948e65ef7481c3bf28f48298d9
pavelpavlov19900-collab <pavelpavlov19900@gmail.com>
2026-04-05 16:47:14 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: 'export',
   distDir: 'out',
+  // ТОВА Е КРИТИЧНО ЗА GITHUB PAGES:
+  basePath: '/billionaire-clock', 
+  assetPrefix: '/billionaire-clock',
   trailingSlash: true,
   images: {
     unoptimized: true,
