7971a5a21c2a50e127a4428539298c1fccedb635
pavelpavlov19900-collab <pavelpavlov19900@gmail.com>
2026-04-05 11:38:22 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
+  output: 'export',
+  distDir: 'out', // Изрично казваме на Next да сложи всичко в папка "out"
   images: {
-    unoptimized: true, 
+    unoptimized: true,
   },
 };
 
